a2f4180f1a37532b4da2b188ec8e7162632c6170
Oleksii Polonchuk <63574876+Alexy-Dev@users.noreply.github.com>
2021-09-15 12:20:44 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,43 @@
 window.addEventListener('DOMContentLoaded', () => {
-    const  btnAboute = document.querySelector('.btn_aboute'),
-           btnProject = document.querySelector('.btn_project'),
-           btnAbouteFoot = document.querySelector('.btn_aboute_foot'),
-           btnProjectFoot = document.querySelector('.btn_project_foot'),
-           btnContact = document.querySelector('.btn_contact'),
+    const  btnAboute = document.querySelectorAll('.btn_aboute'),
+           btnProject = document.querySelectorAll('.btn_project'),           
+           btnContact = document.querySelectorAll('.btn_contact'),
            btnMain = document.querySelector('.btn_main'),         
            form = document.querySelector('.features'),
            formP = document.querySelector('.our-work'),
            formM = document.getElementById('main'),
            formC = document.getElementById('contact');
 
-btnAboute.addEventListener('click', (e) => { 
+btnAboute.forEach(btn => {           
+    btn.addEventListener('click', (e) => { 
     e.preventDefault();   
     form.scrollIntoView({
     behavior: 'smooth'
-});        
+        });        
+    });
 });
-
-btnProject.addEventListener('click', (e) => { 
+btnProject.forEach(btn => {          
+    btn.addEventListener('click', (e) => { 
     e.preventDefault();
     formP.scrollIntoView({
     behavior: 'smooth'
-});        
-});
-btnAbouteFoot.addEventListener('click', (e) => { 
-    e.preventDefault();   
-    form.scrollIntoView({
-    behavior: 'smooth'
-});        
+        });        
+    });
 });
 
-btnProjectFoot.addEventListener('click', (e) => { 
-    e.preventDefault();
-    formP.scrollIntoView({
-    behavior: 'smooth'
-});        
-});
-btnContact.addEventListener('click', (e) => { 
+btnContact.forEach(btn => {       
+    btn.addEventListener('click', (e) => { 
     e.preventDefault();
     formC.scrollIntoView({
      behavior: 'smooth'
-});        
+        });        
+    });
 });
 btnMain.addEventListener('click', (e) => { 
     e.preventDefault();
     formM.scrollIntoView({
     behavior: 'smooth'
-});        
-});
+        });        
+    });
 }
 );
